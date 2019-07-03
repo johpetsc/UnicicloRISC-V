b6c6145900a5722f6be161a1a2e194a68200e6c1
unknown <johpetsc@gmail.com>
2019-07-02 22:24:18 -0300

    Modulo de controle, mux e somador.

---
+++ mux.vhd
@@ -0,0 +1,28 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity mux is
+
+	port(
+		sel	:	in std_logic;
+		A		:	in std_logic_vector(31 downto 0);
+		B		:	in std_logic_vector(31 downto 0);
+		X		:	out std_logic_vector(31 downto 0)
+	);
+end mux;
+
+architecture behavioral of mux is
+
+	begin
+	
+	proc_mux: process (sel)
+	begin
+		if	(sel = '0') then
+			X	<=	A;
+		elsif (sel = '1') then
+			X	<= B;
+		else
+			X 	<= X"00000000";
+		end if;
+	end process;
+end behavioral;
---
+++ somador.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity somador is
+
+
+	port 
+	(
+		A	   : in std_logic_vector  (31 downto 0);
+		B	   : in std_logic_vector  (31 downto 0);
+		Z     : out std_logic_vector(31 downto 0)
+	);
+
+end entity;
+
+architecture behavorial of somador is
+begin
+
+	Z <= std_logic_vector(signed(A) + signed(B));
+
+end behavorial;
